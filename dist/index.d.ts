4c2cc3a37dde2cc5b740a9714ecc94a626823a3f
zhennann <zhen.nann@icloud.com>
2025-02-10 15:52:52 +0800

    feat: update

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+import { App } from "vue";
+export type FunctionAsync<RESULT> = () => Promise<RESULT>;
+export declare const PluginFreeze: {
+    install(app: App): void;
+};
+//# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -58,3 +58,4 @@ export const PluginFreeze = {
         });
     },
 };
+//# sourceMappingURL=index.js.map
