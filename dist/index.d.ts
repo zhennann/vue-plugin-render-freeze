22b6885bdb50ab1f460437ab87146e444ad84178
zhennann <zhen.nann@icloud.com>
2025-02-10 15:53:32 +0800

    feat: update

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { App } from "vue";
-export type FunctionAsync<RESULT> = () => Promise<RESULT>;
-export declare const PluginFreeze: {
-    install(app: App): void;
-};
-//# sourceMappingURL=index.d.ts.map
