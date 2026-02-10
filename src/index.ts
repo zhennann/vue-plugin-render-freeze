01966b4b6b67553334703a4a2efc77c20aaaf372
zhennann <zhen.nann@icloud.com>
2026-02-10 12:00:15 +0800

    feat: update

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { App, ref, withCtx } from "vue";
 export type FunctionAsync<RESULT> = () => Promise<RESULT>;
 
 const SymbolRenderOriginal = Symbol("SymbolRenderOriginal");
-const SymbolRenderPatch = Symbol("SymbolRenderPatch");
+// const SymbolRenderPatch = Symbol("SymbolRenderPatch");
 const SymbolRenderFreezeCounter = Symbol("SymbolRenderFreezeCounter");
 const SymbolRenderFreezeSnapshot = Symbol("SymbolRenderFreezeSnapshot");
 
@@ -17,22 +17,22 @@ export const PluginFreeze = {
         self[SymbolRenderFreezeCounter] = ref(0);
         self[SymbolRenderFreezeSnapshot] = undefined;
         self[SymbolRenderOriginal] = instance[renderMethod];
-        self[SymbolRenderPatch] = function (this, ...args) {
-          // @ts-ignore ignore
-          if (process.env.SERVER) {
-            return withCtx(() => {
-              return self[SymbolRenderOriginal].call(this, ...args);
-            }, instance)();
-          } else {
-            return self[SymbolRenderOriginal].call(this, ...args);
-          }
-        };
+        // self[SymbolRenderPatch] = function (this, ...args) {
+        //   // @ts-ignore ignore
+        //   if (process.env.SERVER) {
+        //     return withCtx(() => {
+        //       return self[SymbolRenderOriginal].call(this, ...args);
+        //     }, instance)();
+        //   } else {
+        //     return self[SymbolRenderOriginal].call(this, ...args);
+        //   }
+        // };
         instance[renderMethod] = function (this, ...args) {
           if (self[SymbolRenderFreezeCounter].value === 0) {
-            return self[SymbolRenderPatch].call(this, ...args);
+            return self[SymbolRenderOriginal].call(this, ...args);
           }
           if (!self[SymbolRenderFreezeSnapshot]) {
-            self[SymbolRenderFreezeSnapshot] = self[SymbolRenderPatch].call(
+            self[SymbolRenderFreezeSnapshot] = self[SymbolRenderOriginal].call(
               this,
               ...args,
             );
